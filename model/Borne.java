0b2c5a1dfaabf8ecec278eeba509d4fbb8b04085
hugoenseirb <Hugo.Suisse_De_Sainte_Claire@bordeaux-inp.fr>
2025-12-01 15:01:36 +0100

    Borne.java

--- model/Borne.java
+++ model/Borne.java
@@ -1,5 +1,45 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Borne {
-    
+    private List<Carte> cartesJ1;
+    private List<Carte> cartesJ2;
+    private boolean revendiquee;
+
+    public Borne() {
+        this.cartesJ1 = new ArrayList<>();
+        this.cartesJ2 = new ArrayList<>();
+        this.revendiquee = false;
+    }
+
+    public void ajouterCarteJ1(Carte c){
+        if (cartesJ1.size() < 3) {
+            cartesJ1.add(c);
+        }
+    }
+    public void ajouterCarteJ2(Carte c){
+        if (cartesJ2.size() < 3) {
+            cartesJ2.add(c);
+        }
+    }
+
+    public List<Carte> getCartesJ1(){
+        return cartesJ1;
+    }
+    public List<Carte> getCartesJ2(){
+        return cartesJ2;
+    }
+
+    public boolean estComplete(){
+        return cartesJ1.size()==3 && cartesJ2.size()==3;
+    }
+
+    public boolean estRevendiquee() {
+        return revendiquee;
+    }
+
+    public void revendiquer() {
+        this.revendiquee = true;
+    }
 }
