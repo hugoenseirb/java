fbabb2ee47745629064e7c02205dec0c7bd58c68
hugoenseirb <Hugo.Suisse_De_Sainte_Claire@bordeaux-inp.fr>
2025-12-01 11:37:06 +0100

    classe carte

--- model/Carte.java
+++ model/Carte.java
@@ -1,5 +1,23 @@
 package model;
  
 public class Carte {
-    
+    private int valeur;
+    private String couleur;
+
+    public Carte(int valeur, String couleur){
+        this.valeur = valeur;
+        this.couleur = couleur;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public String afficherCarte() {
+        return valeur + couleur;
+    }
 }
