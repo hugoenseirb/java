e9a66421317180e71a1d79c4a7243e1141e7f60a
hugoenseirb <Hugo.Suisse_De_Sainte_Claire@bordeaux-inp.fr>
2025-12-03 22:12:11 +0100

    Pioche

--- model/Pioche.java
+++ model/Pioche.java
@@ -1,5 +1,26 @@
 package model;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Pioche {
-    
+    private List<Carte> cartes;
+    public Pioche(List<Carte> cartesInitiales) {
+        this.cartes = new ArrayList<>(cartesInitiales);
+        melanger();
+    }
+    public void melanger(){
+        Collections.shuffle(cartes);
+    }
+    public Carte piocher() {
+        if (cartes.isEmpty()) return null;
+        return cartes.remove(0);
+    }
+    public int taille() {
+        return cartes.size();
+    }
+    public boolean estVide() {
+        return cartes.isEmpty();
+    }
 }
+ 
